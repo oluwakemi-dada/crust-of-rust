7e6bbbdb67d9957f4e8f4e6488449fbec33e7a37
Oluwakemi Dada <dadaoluwakemi97@gmail.com>
2026-08-26 18:17:14 +0100

    feat: add Flatten iterator with basic tests

---
+++ iterators/src/lib.rs
@@ -0,0 +1,54 @@
+pub struct Flatten<O> {
+    outer: O,
+}
+
+impl<O> Flatten<O> {
+    fn new(iter: O) -> Self {
+        Self { outer: iter }
+    }
+}
+
+impl<O> Iterator for Flatten<O>
+where
+    O: Iterator,
+    O::Item: IntoIterator,
+{
+    type Item = <O::Item as IntoIterator>::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.outer.next().and_then(|inner| inner.into_iter().next())
+    }
+}
+
+pub fn flatten<I>(iter: I) -> Flatten<I> {
+    Flatten::new(iter)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty() {
+        assert_eq!(flatten(std::iter::empty::<Vec<()>>()).count(), 0)
+    }
+
+    fn empty_wide() {
+        assert_eq!(flatten(vec![Vec::<()>::new(), vec![], vec![]].into_iter()).count(), 0)
+    }
+
+    #[test]
+    fn one() {
+        assert_eq!(flatten(std::iter::once(vec!["a"])).count(), 1)
+    }
+
+    #[test]
+    fn two() {
+        assert_eq!(flatten(std::iter::once(vec!["a", "b"])).count(), 2)
+    }
+
+    #[test]
+    fn two_wide() {
+        assert_eq!(flatten(vec![vec!["a"], vec!["b"]].into_iter()).count(), 2)
+    }
+}
